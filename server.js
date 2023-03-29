451646dfdad10afc9037317dc39860fd28ddac1c
Rafael <ochoacabriles@gmail.com>
2023-03-28 18:03:25 -0600

    Add ch-calculator-example dependency

--- server.js
+++ server.js
@@ -5,7 +5,34 @@ const { port } = require('./config/environment');
 const { handleMessagesEndpoints } = require('./helpers/handleMessageEndpoints');
 const { sendMail } = require('./helpers/sendEmail');
 
+const { add, subtract, multiply, divide } = require('ch-calculator-example');
+
 const app = express();
+
+app.get('/calculator', (req, res) => {
+  const { operation, a, b } = req.query;
+
+  const validOperations = ['add', 'subtract', 'multiply', 'divide'];
+  if (
+    !validOperations.includes(operation) ||
+    a === undefined ||
+    b === undefined ||
+    (operation === 'divide' && a === 0)
+  ) {
+    res.status(404).send(`Bad params`);
+    return;
+  }
+
+  if (operation === 'add') {
+    res.send(`a + b = ${add(a, b)}`);
+  } else if (operation === 'subtract') {
+    res.send(`b - a = ${subtract(a, b)}`);
+  } else if (operation === 'multiply') {
+    res.send(`a * b = ${multiply(a, b)}`);
+  } else if (operation === 'divide') {
+    res.send(`b / a = ${divide(a, b)}`);
+  }
+});
 
 app.get('/sms', async (req, res) => {
   handleMessagesEndpoints('sms', req, res);
